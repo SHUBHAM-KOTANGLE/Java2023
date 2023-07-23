caa652f3dadf826d1663024db41042522d27c6a4
shubham <tl@gmail.com>
2023-07-23 23:35:53 +0530

    1 add

--- src/com/codeo/StarPattern/Starpatternmid.java
+++ src/com/codeo/StarPattern/Starpatternmid.java
@@ -93,6 +93,36 @@ public class Starpatternmid {
 			}
 			System.out.println();
 		}
+		System.out.println("-------------------------------------------13");
+		for(int i=1;i<=4;i++) 
+		{
+			for(int j=3;j>=i;j--) 
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<i*2;k++) 
+			{
+				if(k>1 && k<i*2-1) 
+				{
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			
+			
+			System.out.println();
+		}
+		
+			
+				
+			
+		
+		
+		
+		
+		
+		
 	}
 
 }
